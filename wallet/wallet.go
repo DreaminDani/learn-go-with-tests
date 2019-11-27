054692d07b91e7d2a0faf97daf3a75843cf02380
Daniel Sandoval <daniel@desandoval.net>
2019-11-27 05:52:51 -0500

    WIP: need to reject insufficient funds

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -14,6 +14,12 @@ type Wallet struct {
 	balance Bitcoin
 }
 
+// Withdraw will remove some bitcoin from a wallet
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	w.balance -= amount
+	return nil
+}
+
 // Deposit will add some Bitcoin to a wallet
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -5,16 +5,35 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-
-	wallet := Wallet{}
-
-	wallet.Deposit(Bitcoin(10))
-
-	got := wallet.Balance()
-
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+	verifyBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		verifyBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(10)
+		verifyBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(9)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(10)
+
+		verifyBalance(t, wallet, Bitcoin(startingBalance))
+
+		if err == nil {
+			t.Error("Wanted an error but didn't get one")
+		}
+	})
 }
